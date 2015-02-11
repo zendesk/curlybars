4e590450ff17b48d64db8e1071b3c94945b05be1
Libo Cannici <lcannici@zendesk.com>
2015-02-11 13:56:02 +0100

    Cleaned up Lexer

--- lib/curly_bars/lexer.rb
+++ lib/curly_bars/lexer.rb
@@ -2,7 +2,6 @@ require 'rltk/lexer'
 
 module CurlyBars
   class Lexer < RLTK::Lexer
-
     rule(/{{!--/) { push_state :comment_block }
     rule(/.*?(?=--}})/m, :comment_block)
     rule(/--}}/, :comment_block) { pop_state }
@@ -11,54 +10,28 @@ module CurlyBars
     rule(/.*?(?=}})/m, :comment)
     rule(/}}/, :comment) { pop_state }
 
-    rule /{{/, :default do
-      push_state :expression
-      :CURLYSTART
-    end
-
-    rule /}}/, :expression do
-      pop_state
-      :CURLYEND
-    end
+    rule(/{{/) { push_state :expression; :CURLYSTART }
+    rule(/}}/, :expression) { pop_state; :CURLYEND }
 
     rule(/#if(?=\s)/, :expression) { :IF }
     rule(/\/\s*if/, :expression) { :ENDIF }
 
-    rule(/\s/, :expression)
+    rule(/#unless(?=\s)/, :expression) { :UNLESS }
+    rule(/\/unless/, :expression) { :UNLESSCLOSE }
 
-    rule /(#[A-Za-z][\w\.]*\??)/, :expression do |name|
-      :HELPER
-    end
+    rule(/#each\s+/, :expression) { :EACH }
+    rule(/\/each/, :expression) { :EACHCLOSE }
 
-    rule /#unless\s+/, :expression do
-      :UNLESS
-    end
+    rule(/#with\s+/, :expression) { :WITH }
+    rule(/\/with/, :expression) { :WITHCLOSE }
 
-    rule /\/unless/, :expression do
-      :UNLESSCLOSE
-    end
+    rule(/else/, :expression) { :ELSE }
 
-    rule /#each\s+/, :expression do
-      :EACH
-    end
-
-    rule /\/each/, :expression do
-      :EACHCLOSE
-    end
-
-    rule /#with\s+/, :expression do
-      :WITH
-    end
-
-    rule /\/with/, :expression do
-      :WITHCLOSE
-    end
-
-    rule /else/, :expression do
-      :ELSE
-    end
+    rule(/(#[A-Za-z][\w\.]*\??)/, :expression) { |helper| [:HELPER, helper] }
 
     rule(/[A-Za-z][\w\.]*\??/, :expression) { |name| [:PATH, name] }
+
+    rule(/\s/, :expression)
 
     rule(/.*?(?={{|\z)/m) { |text| [:TEXT, text] }
   end
