57ab9383a6f4f1c4fa080738edf53e2b6ccc2712
Libo Cannici <lcannici@zendesk.com>
2015-02-14 10:21:19 +0100

    Removed module CompilationSupport

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,33 +6,3 @@ require 'rspec/rails'
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 end
-
-module CompilationSupport
-  def define_presenter(name = "ShowPresenter", &block)
-    presenter_class = Class.new(Curly::Presenter, &block)
-    stub_const(name, presenter_class)
-    presenter_class
-  end
-
-  def render(source, locals = {}, presenter_class = nil, &block)
-    if presenter_class.nil?
-      unless defined?(ShowPresenter)
-        define_presenter("ShowPresenter")
-      end
-
-      presenter_class = ShowPresenter
-    end
-
-    identifier = "show"
-    handler = Curly::TemplateHandler
-    details = { virtual_path: 'show' }
-    template = ActionView::Template.new(source, identifier, handler, details)
-    view = ActionView::Base.new
-
-    begin
-      template.render(view, locals, &block)
-    rescue ActionView::Template::Error => e
-      raise e.original_exception
-    end
-  end
-end
