9ba85bca8019477e56afcc90cf040c34e19cb33d
Libo Cannici <lcannici@zendesk.com>
2015-02-16 13:46:26 +0100

    WIP

--- lib/curlybars/node/path.rb
+++ lib/curlybars/node/path.rb
@@ -4,12 +4,17 @@ module Curlybars
       def compile
         <<-RUBY
           begin
-            "#{path}".split(/\\./).inject(contexts.last) do |memo, m|
+            chain = "#{path}".split(/\\./)
+            method_to_return = chain.pop
+            resolved = chain.inject(contexts.last) do |memo, m|
               if memo.respond_to?(m.to_sym)
                 memo.public_send(m.to_sym)
               else
                 raise "Template error: context " + memo.class.to_s + " doesn't implement: " << m
               end
+            end
+            if resolved.respond_to?(m.to_sym)
+              resolved.method(method_to_return.to_sym)
             end
           end
         RUBY
