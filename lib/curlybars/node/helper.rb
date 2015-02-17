ec24c8082b370a8f77c0106e98fa881cfcd36bd8
Mauro Codella <mcodella@zendesk.com>
2015-02-17 10:32:35 +0100

    Helper node test coverage

--- lib/curlybars/node/helper.rb
+++ lib/curlybars/node/helper.rb
@@ -1,7 +1,7 @@
 module Curlybars
   module Node
     class Helper
-      attr_reader :path, :context, :options
+      attr_reader :helper, :context, :options
 
       class DefaultContext
         def compile
@@ -9,26 +9,23 @@ module Curlybars
         end
       end
 
-      def initialize(path, context, options)
-        @path = path
+      def initialize(helper, context, options)
+        @helper = helper
         @context = context || DefaultContext.new
         @options = options || []
       end
 
       def compile
-        compiled_options = <<-RUBY
-          options = ActiveSupport::HashWithIndifferentAccess.new
-        RUBY
-
-        compiled_options << options.map do |option|
+        compiled_options = options.map do |option|
           "options.merge!(#{option.compile})"
         end.join("\n")
 
         <<-RUBY
+          options = ActiveSupport::HashWithIndifferentAccess.new
           #{compiled_options}
           ActiveSupport::SafeBuffer.new begin
             context = #{context.compile}.call
-            helper = #{path.compile}
+            helper = #{helper.compile}
             helper.call(*([context, options].compact.first(helper.arity))) do
               raise "You cannot yield a block from within a helper. Use a block helper instead."
             end
