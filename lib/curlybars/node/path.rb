14a314eb7d474e3205d721611dbf01e17bcc30f5
Libo Cannici <lcannici@zendesk.com>
2015-02-17 11:22:41 +0100

    Check both withlisting and #respond_to?

--- lib/curlybars/node/path.rb
+++ lib/curlybars/node/path.rb
@@ -7,12 +7,12 @@ module Curlybars
             chain = "#{path}".split(/\\./)
             method_to_return = chain.pop
             resolved = chain.inject(contexts.last) do |memo, m|
-              unless memo.class.allows_method?(m.to_sym)
+              if !memo.class.allows_method?(m.to_sym) || !memo.respond_to?(m.to_sym)
                 raise "Template error: context " + memo.class.to_s + " doesn't implement: " << m
               end
               memo.public_send(m.to_sym)
             end
-            unless resolved.class.allows_method?(method_to_return.to_sym)
+            if !resolved.class.allows_method?(method_to_return.to_sym) || !resolved.respond_to?(method_to_return.to_sym)
               raise "Template error: context " + resolved.class.to_s + " doesn't implement: " << method_to_return
             end
             resolved.method(method_to_return.to_sym)
