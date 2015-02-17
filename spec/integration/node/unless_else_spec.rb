c2db8bc07726f48faa5f659ad3d4d6057cd44cf6
Mauro Codella <mcodella@zendesk.com>
2015-02-17 15:12:24 +0100

    Add UnlessElse spec

---
+++ spec/integration/node/unless_else_spec.rb
@@ -0,0 +1,38 @@
+describe "unless-else block" do
+  let(:post) { double("post") }
+  let(:presenter) { IntegrationTest::Presenter.new(double("view_context"), post: post) }
+
+  it "renders the unless_template" do
+    IntegrationTest::Presenter.stub(:allows_method?).with(:condition) { true }
+    presenter.stub(:condition) { false }
+
+    template = compile(<<-HBS.strip_heredoc)
+      {{#unless condition}}
+        unless_template
+      {{else}}
+        else_template
+      {{/unless}}
+    HBS
+
+    expect(eval(template)).to resemble(<<-HTML.strip_heredoc)
+      unless_template
+    HTML
+  end
+
+  it "renders the else_template" do
+    IntegrationTest::Presenter.stub(:allows_method?).with(:condition) { true }
+    presenter.stub(:condition) { true }
+
+    template = compile(<<-HBS.strip_heredoc)
+      {{#unless condition}}
+        unless_template
+      {{else}}
+        else_template
+      {{/unless}}
+    HBS
+
+    expect(eval(template)).to resemble(<<-HTML.strip_heredoc)
+      else_template
+    HTML
+  end
+end
