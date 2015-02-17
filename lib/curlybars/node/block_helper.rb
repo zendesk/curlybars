7bc723edf419677f1cd7055bdab1b8f77e020ec7
Mauro Codella <mcodella@zendesk.com>
2015-02-17 11:14:03 +0100

    Cosmetics

--- lib/curlybars/node/block_helper.rb
+++ lib/curlybars/node/block_helper.rb
@@ -2,24 +2,17 @@ require 'curlybars/error/incorrect_ending_error'
 
 module Curlybars
   module Node
-    class BlockHelper
-      attr_reader :helper, :context, :options, :template, :helperclose
-
+    BlockHelper = Struct.new(:helper, :context, :options, :template, :helperclose) do
       def initialize(helper, context, options, template, helperclose)
         if helper != helperclose
           raise Curlybars::Error::IncorrectEndingError,
             "block `#{helper}` cannot be closed by `#{helperclose}`"
         end
-
-        @helper = helper
-        @context = context
-        @options = options || []
-        @template = template
-        @helperclose = helperclose
+        super
       end
 
       def compile
-        compiled_options = options.map do |option|
+        compiled_options = (options || []).map do |option|
           "options.merge!(#{option.compile})"
         end.join("\n")
 
--- lib/curlybars/node/helper.rb
+++ lib/curlybars/node/helper.rb
@@ -1,22 +1,8 @@
 module Curlybars
   module Node
-    class Helper
-      attr_reader :helper, :context, :options
-
-      class DefaultContext
-        def compile
-          "->{}"
-        end
-      end
-
-      def initialize(helper, context, options)
-        @helper = helper
-        @context = context || DefaultContext.new
-        @options = options || []
-      end
-
+    Helper = Struct.new(:helper, :context, :options) do
       def compile
-        compiled_options = options.map do |option|
+        compiled_options = (options || []).map do |option|
           "options.merge!(#{option.compile})"
         end.join("\n")
 
@@ -24,13 +10,19 @@ module Curlybars
           options = ActiveSupport::HashWithIndifferentAccess.new
           #{compiled_options}
           ActiveSupport::SafeBuffer.new begin
-            context = #{context.compile}.call
+            context = #{(context || DefaultContext.new).compile}.call
             helper = #{helper.compile}
             helper.call(*([context, options].compact.first(helper.arity))) do
               raise "You cannot yield a block from within a helper. Use a block helper instead."
             end
           end
         RUBY
+      end
+
+      class DefaultContext
+        def compile
+          "->{}"
+        end
       end
     end
   end
