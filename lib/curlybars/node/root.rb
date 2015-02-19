57b5c0865581784cc3db409965fb100e346fabca
Mauro Codella <mcodella@zendesk.com>
2015-02-19 17:05:17 +0100

    Test HBS class

--- lib/curlybars/node/root.rb
+++ lib/curlybars/node/root.rb
@@ -4,14 +4,12 @@ module Curlybars
       def compile
         <<-RUBY
           contexts = [presenter]
-          hbs = #{hbs}.new(contexts)
+          hbs = #{self.class.hbs}.new(contexts)
           #{template.compile}
         RUBY
       end
 
-      private
-
-      def hbs
+      def self.hbs
         <<-RUBY
           Struct.new(:contexts) do
             def to_bool(condition)
@@ -25,16 +23,27 @@ module Curlybars
             def path(path)
               chain = path.split(/\\./)
               method_to_return = chain.pop
-              resolved = chain.inject(contexts.last) do |memo, m|
-                if !memo.class.allows_method?(m.to_sym) || !memo.respond_to?(m.to_sym)
-                  raise "Template error: context " + memo.class.to_s + " doesn't implement: " << m
-                end
-                memo.public_send(m.to_sym)
+
+              resolved = chain.inject(contexts.last) do |context, meth|
+                raise_if_not_traversable(context, meth)
+                context.public_send(meth)
               end
-              if !resolved.class.allows_method?(method_to_return.to_sym) || !resolved.respond_to?(method_to_return.to_sym)
-                raise "Template error: context " + resolved.class.to_s + " doesn't implement: " << method_to_return
-              end
+
+              raise_if_not_traversable(resolved, method_to_return)
               resolved.method(method_to_return.to_sym)
+            end
+
+            private
+
+            def raise_if_not_traversable(context, meth)
+              accessor = meth.to_sym
+
+              context_allows_method = context.class.allows_method?(accessor)
+              context_responds_to_method = context.respond_to?(accessor)
+
+              unless context_allows_method && context_responds_to_method
+                raise context.class.to_s + " doesn't allow `" + meth + "`"
+              end
             end
           end
         RUBY
