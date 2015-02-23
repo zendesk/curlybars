0ca80df576ecb2e050afa6d17461cbbf3a8a99f7
Mauro Codella <mcodella@zendesk.com>
2015-02-23 10:46:09 +0100

    Add INTEGER and BOOLEAN

--- lib/curlybars/lexer.rb
+++ lib/curlybars/lexer.rb
@@ -28,8 +28,10 @@ module Curlybars
     r(/with(?![A-Za-z_])/, :curly) { :WITH }
     r(/else(?![A-Za-z_])/, :curly) { :ELSE }
     r(/([A-Za-z_]\w*)\s*=/, :curly) { |key| [:KEY, match[1]] }
+    r(/true/, :curly) { |string| [:BOOLEAN, true] }
+    r(/false/, :curly) { |string| [:BOOLEAN, false] }
     r(/[A-Za-z_][\w\.]*\??/, :curly) { |name| [:PATH, name] }
-
+    r(/\d+/, :curly) { |integer| [:INTEGER, integer.to_i] }
     r(/'(.*?)'/m, :curly) { |string| [:STRING, match[1]] }
     r(/"(.*?)"/m, :curly) { |string| [:STRING, match[1]] }
 
