b5e1d16b2cedde296942e84464c15db13b8e35a6
Cristian Planas <cplanas@zendesk.com>
2015-02-26 11:17:15 +0100

    Context check in With nodes has its own method

--- lib/curlybars/node/with.rb
+++ lib/curlybars/node/with.rb
@@ -5,17 +5,25 @@ module Curlybars
         <<-RUBY
           compiled_path = #{path.compile}.call
 
-          unless compiled_path.class.respond_to? :allows_method?
-            position = hbs.position(#{position.line_number}, #{position.line_offset})
-            message = "`#{path.path}` is not a context type object"
-            raise Curlybars::Error::Render.new('context_is_not_a_presenter', message, position)
-          end
+          #{check_context_is_presenter}
 
           contexts << compiled_path
           begin
             buffer.safe_concat(#{template.compile})
           ensure
             contexts.pop
+          end
+        RUBY
+      end
+
+      private
+
+      def check_context_is_presenter
+        <<-RUBY
+          unless compiled_path.class.respond_to? :allows_method?
+            position = hbs.position(#{position.line_number}, #{position.line_offset})
+            message = "`#{path.path}` is not a context type object"
+            raise Curlybars::Error::Render.new('context_is_not_a_presenter', message, position)
           end
         RUBY
       end
