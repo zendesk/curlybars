af64cab981741a51e2b37605dc2c3de079dcaa72
Libo Cannici <lcannici@zendesk.com>
2015-03-03 21:28:23 +0100

    Implement dependency tree for static validation

--- lib/curlybars/method_whitelist.rb
+++ lib/curlybars/method_whitelist.rb
@@ -1,8 +1,44 @@
 module Curlybars
   module MethodWhitelist
-    def allow_methods(*methods)
+    def allow_methods(*methods, **methods_with_type)
+      methods_with_type.each do |method_with_type|
+        method_name = method_with_type.first
+        type = method_with_type.last
+
+        if type.is_a?(Array)
+          if type.size != 1 || !type.first.respond_to?(:dependency_tree)
+            raise "Invalid allowed method syntax for `#{method_name}`. Collections must be of one presenter class"
+          end
+        end
+      end
+
       define_method(:allowed_methods) do
-        defined?(super) ? super() + methods : methods
+        methods_list = methods + methods_with_type.keys
+        defined?(super) ? super() + methods_list : methods_list
+      end
+
+      define_singleton_method(:methods_schema) do
+        methods.inject({}) do |memo, method|
+          memo[method] = nil
+          memo
+        end.merge(methods_with_type)
+      end
+
+      define_singleton_method(:dependency_tree) do
+        methods_schema.inject({}) do |memo, method_with_type|
+          method_name = method_with_type.first
+          type = method_with_type.last
+
+          if type.respond_to?(:dependency_tree)
+            memo[method_name] = type.dependency_tree
+          elsif type.is_a?(Array)
+            memo[method_name] = [type.first.dependency_tree]
+          else
+            memo[method_name] = type
+          end
+
+          memo
+        end
       end
 
       define_method(:allows_method?) do |method|
--- spec/method_whitelist_spec.rb
+++ spec/method_whitelist_spec.rb
@@ -9,21 +9,41 @@ describe Curlybars::MethodWhitelist do
 
   describe ".allow_methods" do
     before do
+      link_presenter = Class.new
+      article_presenter = Class.new
+
       dummy_class.class_eval do
-        allow_methods :cook
+        allow_methods :cook, link: link_presenter, article: article_presenter
       end
     end
 
     it "should set the allowed methods" do
-      expect(dummy_class.new.allowed_methods).to eq([:cook])
+      expect(dummy_class.new.allowed_methods).to eq([:cook, :link, :article])
+    end
+
+    it "raises when collection is not of presenters" do
+      expect do
+        dummy_class.class_eval { allow_methods :cook, links: ["foobar"] }
+      end.to raise_error(RuntimeError)
+    end
+
+    it "raises when collection cardinality is greater than one" do
+      stub_const("OnePresenter", Class.new { extend Curlybars::MethodWhitelist })
+      stub_const("OtherPresenter", Class.new { extend Curlybars::MethodWhitelist })
+
+      expect do
+        dummy_class.class_eval { allow_methods :cook, links: [OnePresenter, OtherPresenter] }
+      end.to raise_error(RuntimeError)
     end
   end
 
   describe "inheritance and composition" do
     let (:base_presenter) do
+      link_presenter = Class.new
+
       Class.new do
         extend Curlybars::MethodWhitelist
-        allow_methods :cook
+        allow_methods :cook, link: link_presenter
       end
     end
 
@@ -46,7 +66,54 @@ describe Curlybars::MethodWhitelist do
     end
 
     it "should allow methods from inheritance and composition" do
-      expect(post_presenter.new.allowed_methods).to eq([:cook, :form, :wave])
+      expect(post_presenter.new.allowed_methods).to eq([:cook, :link, :form, :wave])
     end
+  end
+
+  describe ".methods_schema" do
+    before do
+      stub_const("ArticlePresenter", Class.new { extend Curlybars::MethodWhitelist })
+      stub_const("LinkPresenter", Class.new { extend Curlybars::MethodWhitelist })
+
+      dummy_class.class_eval do
+        allow_methods :cook, links: [LinkPresenter], article: ArticlePresenter
+      end
+    end
+
+    it "should setup a schema of methods" do
+      expect(dummy_class.methods_schema).
+        to eq({ cook: nil, links: [LinkPresenter], article: ArticlePresenter })
+    end
+  end
+
+  describe ".dependency_tree" do
+    before do
+      link_presenter = Class.new do
+        extend Curlybars::MethodWhitelist
+        allow_methods :url
+      end
+
+      article_presenter = Class.new do
+        extend Curlybars::MethodWhitelist
+        allow_methods :title, :body
+      end
+
+      stub_const("ArticlePresenter", article_presenter)
+      stub_const("LinkPresenter", link_presenter)
+
+      dummy_class.class_eval do
+        allow_methods :cook, links: [LinkPresenter], article: ArticlePresenter
+      end
+    end
+
+    it "should return a dependencies tree" do
+      expect(dummy_class.dependency_tree).
+        to eq({
+          cook: nil,
+          links: [{ url: nil }],
+          article: { title: nil, body: nil}
+        })
+    end
+
   end
 end
