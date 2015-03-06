51f280a02860c0afe9b7aed1e9557396e73c3bfd
Libo Cannici <lcannici@zendesk.com>
2015-03-06 13:35:58 +0100

    Move namespacing to the Curlybars::Presenter

--- lib/curlybars/presenter.rb
+++ lib/curlybars/presenter.rb
@@ -3,5 +3,10 @@ require 'curlybars/method_whitelist'
 module Curlybars
   class Presenter < Curly::Presenter
     extend Curlybars::MethodWhitelist
+
+    def self.presenter_for_path(path)
+      name_space = Curlybars.configuration.presenters_namespace
+      super(File.join(name_space, path))
+    end
   end
 end
