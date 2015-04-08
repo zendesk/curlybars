de1905e8307b878b5b71e6077a4577a07035a49f
Mauro Codella <mcodella@zendesk.com>
2015-04-08 14:25:02 +0200

    Better guards

--- lib/curlybars/safe_buffer.rb
+++ lib/curlybars/safe_buffer.rb
@@ -5,7 +5,7 @@ module Curlybars
     end
 
     def safe_concat(buffer)
-      unless (length + buffer.length) < Curlybars.configuration.output_limit
+      if (length + buffer.length) > Curlybars.configuration.output_limit
         message = "Output too long (> %s bytes)" % Curlybars.configuration.output_limit
         raise Curlybars::Error::Render.new('output_too_long', message, nil)
       end
--- spec/curlybars/safe_buffer_spec.rb
+++ spec/curlybars/safe_buffer_spec.rb
@@ -6,30 +6,30 @@ describe Curlybars::SafeBuffer do
   end
 
   describe "#safe_concat" do
-    it "accepts when (buffer length + the existing output lenght) < output_limit" do
+    it "accepts when (buffer length + the existing output lenght) <= output_limit" do
       allow(configuration).to receive(:output_limit) { 10 }
 
       buffer = Curlybars::SafeBuffer.new('*' * 5)
 
       expect do
-        buffer.safe_concat('*' * 4)
+        buffer.safe_concat('*' * 5)
       end.not_to raise_error
     end
 
-    it "raises when (buffer length + the existing output lenght) >= output_limit" do
+    it "raises when (buffer length + the existing output lenght) > output_limit" do
       allow(configuration).to receive(:output_limit) { 10 }
-      buffer = Curlybars::SafeBuffer.new('*' * 9)
+      buffer = Curlybars::SafeBuffer.new('*' * 10)
 
       expect do
         buffer.safe_concat('*')
       end.to raise_error(Curlybars::Error::Render)
     end
 
-    it "raises when buffer length >= output_limit" do
+    it "raises when buffer length > output_limit" do
       allow(configuration).to receive(:output_limit) { 10 }
 
       expect do
-        Curlybars::SafeBuffer.new.safe_concat('*' * 10)
+        Curlybars::SafeBuffer.new.safe_concat('*' * 11)
       end.to raise_error(Curlybars::Error::Render)
     end
   end
