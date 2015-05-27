74a32065997c3b45accdcf3b77e72accae9dc5a1
Mauro Codella <mcodella@zendesk.com>
2015-05-27 10:33:47 +0200

    Remove useless unit tests for nodes

--- spec/curlybars/node/partial_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Curlybars::Node::Partial do
-  it "compiles path correctly" do
-    path = double(:path)
-
-    expect(path).to receive(:compile)
-
-    Curlybars::Node::Partial.new(path).compile
-  end
-end
