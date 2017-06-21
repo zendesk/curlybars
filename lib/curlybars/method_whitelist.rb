fdf689ba4f400c8d52805e20cad71a1b116ae4ee
Ilkka Oksanen <iao@iki.fi>
2017-06-21 23:18:24 +0200

    Support setting allow_methods at runtime
    This commit allows the consumer of Curlybars to use dynamic allow_methods
    definitions, e.g in the presenter:
    
    ```
    allow methods settings: -> (env) { Hash[env[:options].map{ |a| [a, nil] }] }
    ```
    
    Provided proc is responsible to return the dependency subtree under 'settings'.

--- lib/curlybars/method_whitelist.rb
+++ lib/curlybars/method_whitelist.rb
@@ -1,10 +1,7 @@
 module Curlybars
   module MethodWhitelist
     def allow_methods(*methods, **methods_with_type)
-      methods_with_type.each do |method_with_type|
-        method_name = method_with_type.first
-        type = method_with_type.last
-
+      methods_with_type.each do |(method_name, type)|
         if type.is_a?(Array)
           if type.size != 1 || !type.first.respond_to?(:dependency_tree)
             raise "Invalid allowed method syntax for `#{method_name}`. Collections must be of one presenter class"
@@ -17,34 +14,42 @@ module Curlybars
         defined?(super) ? super() + methods_list : methods_list
       end
 
-      define_singleton_method(:methods_schema) do
+      define_singleton_method(:methods_schema) do |*args|
         schema = methods.each_with_object({}) do |method, memo|
           memo[method] = nil
         end
 
-        schema.merge!(methods_with_type)
+        methods_with_type_resolved = methods_with_type.each_with_object({}) do |(method_name, type), memo|
+          memo[method_name] = if type.respond_to?(:call)
+            type.call(*args)
+          else
+            type
+          end
+        end
+
+        schema.merge!(methods_with_type_resolved)
 
         # Inheritance
-        schema.merge!(super()) if defined?(super)
+        schema.merge!(super(*args)) if defined?(super)
 
         # Included modules
         included_modules.each do |mod|
           next unless mod.respond_to?(:methods_schema)
-          schema.merge!(mod.methods_schema)
+          schema.merge!(mod.methods_schema(*args))
         end
 
         schema
       end
 
-      define_singleton_method(:dependency_tree) do
-        methods_schema.each_with_object({}) do |method_with_type, memo|
+      define_singleton_method(:dependency_tree) do |*args|
+        methods_schema(*args).each_with_object({}) do |method_with_type, memo|
           method_name = method_with_type.first
           type = method_with_type.last
 
           memo[method_name] = if type.respond_to?(:dependency_tree)
-            type.dependency_tree
+            type.dependency_tree(*args)
           elsif type.is_a?(Array)
-            [type.first.dependency_tree]
+            [type.first.dependency_tree(*args)]
           else
             type
           end
